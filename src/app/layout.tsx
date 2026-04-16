84cdaf5b7b7845fd876646bc7434865a04a300da
Quentin <Quentin@macbookpro.mynetworksettings.com>
2026-04-16 18:54:37 -0400

    Add Google Analytics (G-V65FBEY2NK)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const cormorant = Cormorant_Garamond({
@@ -131,6 +132,13 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-screen bg-white text-black antialiased" suppressHydrationWarning>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-V65FBEY2NK" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-V65FBEY2NK');
+        `}</Script>
         {children}
       </body>
     </html>
