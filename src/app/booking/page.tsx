84d07d706ac2d530c93689644cfd9bc8e7f33547
Quentin <Quentin@macbookpro.mynetworksettings.com>
2026-05-04 17:30:19 -0400

    Add /booking redirect to Square appointments

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function BookingRedirect() {
+  redirect("https://square.site/appointments/buyer/widget/qe4tfv3078b5gx/LYH1D5CHJ3Q63");
+}
